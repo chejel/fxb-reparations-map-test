b87fc287abe3a36d827fe3c98edaf8440e257b00
chejel <chejenj@gmail.com>
2024-02-29 00:33:00 -0500

    Add About panel

---
+++ src/lib/components/panels/About.svelte
@@ -0,0 +1,38 @@
+<script>
+</script>
+
+<main>
+	<section class="introduction">
+		<p>
+			Viverra aliquet eget sit amet tellus cras adipiscing. Nullam ac tortor vitae purus. Mauris
+			rhoncus aenean vel elit. Et tortor consequat id porta nibh venenatis. Et magnis dis parturient
+			montes nascetur ridiculus mus. Dictumst quisque sagittis purus sit. Eget arcu dictum varius
+			duis.
+		</p>
+	</section>
+	<section class="stats">
+		<h2>Stats</h2>
+		<hr />
+		<p>
+			Viverra aliquet eget sit amet tellus cras adipiscing. Nullam ac tortor vitae purus. Mauris
+			rhoncus aenean vel elit. Et tortor consequat id porta nibh venenatis. Et magnis dis parturient
+			montes nascetur ridiculus mus. Dictumst quisque sagittis purus sit. Eget arcu dictum varius
+			duis.
+		</p>
+	</section>
+</main>
+
+<style>
+	h2 {
+		margin-top: 1rem;
+	}
+
+	.stats {
+		margin-top: 1rem;
+		background-color: rgba(255, 255, 253, 0.75);
+		/* border: 1px solid rgba(236, 194, 122, 0.5); */
+		border: 0.75px solid #ffb347;
+		padding: 0 0.75rem 0.75rem;
+		border-radius: 5px;
+	}
+</style>
