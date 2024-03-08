cbe94ad98ff4a0a5353714ce7c49ea8d6aa4aa70
chejel <chejenj@gmail.com>
2024-03-07 23:26:05 -0500

    Add stats card

--- src/lib/components/panels/About.svelte
+++ src/lib/components/panels/About.svelte
@@ -1,23 +1,50 @@
 <script>
+	// Import components
+	import Stats from '$lib/components/panels/Stats.svelte';
+
+	// Import icon components
+	import StatsIcon from '$lib/components/icons/Stats.svelte';
+
+	// Import stores
+	import { reparationsData } from '$lib/stores.js';
 </script>
 
-<main>
-	<section class="introduction">
-		<p>
-			Viverra aliquet eget sit amet tellus cras adipiscing. Nullam ac tortor vitae purus. Mauris
-			rhoncus aenean vel elit. Et tortor consequat id porta nibh venenatis. Et magnis dis parturient
-			montes nascetur ridiculus mus. Dictumst quisque sagittis purus sit. Eget arcu dictum varius
-			duis.
-		</p>
-	</section>
+<section class="introduction">
+	<p>
+		Viverra aliquet eget sit amet tellus cras adipiscing. Nullam ac tortor vitae purus. Mauris
+		rhoncus aenean vel elit. Et tortor consequat id porta nibh venenatis. Et magnis dis parturient
+		montes nascetur ridiculus mus. Dictumst quisque sagittis purus sit. Eget arcu dictum varius
+		duis.
+	</p>
+</section>
+{#if $reparationsData}
 	<section class="card">
-		<h2>Stats</h2>
-		<hr />
-		<p>
-			Viverra aliquet eget sit amet tellus cras adipiscing. Nullam ac tortor vitae purus. Mauris
-			rhoncus aenean vel elit. Et tortor consequat id porta nibh venenatis. Et magnis dis parturient
-			montes nascetur ridiculus mus. Dictumst quisque sagittis purus sit. Eget arcu dictum varius
-			duis.
-		</p>
+		<p class="card-header"><StatsIcon /> Represented</p>
+		<!-- <hr /> -->
+		<div class="stats-table">
+			<Stats />
+		</div>
 	</section>
-</main>
+{/if}
+
+<style>
+	.card-header {
+		font-family: 'Barlow Condensed', sans-serif;
+		font-size: 1rem;
+		text-transform: uppercase;
+		font-weight: 600;
+		background-color: rgba(234, 230, 230, 0.7);
+		padding: 0.6rem 0.75rem;
+		display: flex;
+		gap: 5px;
+	}
+
+	.stats-table {
+		/* margin-top: 0.5rem; */
+		padding: 0.5rem 0.75rem;
+	}
+
+	/* hr {
+		border-top: 1px solid rgba(45, 45, 45, 0.35);
+	} */
+</style>
---
+++ src/lib/components/panels/Stats.svelte
@@ -0,0 +1,224 @@
+<script>
+	// Import stores
+	import { reparationsData, reparationsStateData } from '$lib/stores.js';
+
+	// # of cities
+	const numCities = $reparationsData?.length;
+
+	// # of counties
+	const numCounties = '#';
+
+	// # of states
+	const numStates = '#';
+
+	// # with reports
+	const numReports = $reparationsData?.filter((d) =>
+		d.properties['Has the city released a report on reparations?'].includes('Yes')
+	).length;
+
+	// # with funding
+	const numFunding = $reparationsData?.filter((d) =>
+		d.properties['Has the city approved reparations funding?'].includes('Yes')
+	).length;
+
+	// # with funding source
+	const numSource = $reparationsData?.filter((d) =>
+		d.properties['Has the city decided where funding will come from?'].includes('Yes')
+	).length;
+
+	// # with allocation
+	const numAllocation = $reparationsData?.filter((d) =>
+		d.properties['Has the city begun allocating reparations?'].includes('Yes')
+	).length;
+
+	// # with direct payments
+	const numDirect = $reparationsData?.filter((d) =>
+		d.properties['Has the city determined if direct payments will be included?'].includes('Yes')
+	).length;
+
+	// # with eligibility determined
+	const numEligibility = $reparationsData?.filter((d) =>
+		d.properties['Has the city determined who is eligible to receive direct payments?'].includes(
+			'Yes'
+		)
+	).length;
+</script>
+
+<section class="location-stats">
+	<div>
+		<span class="number top-number">{numCities}</span> <span class="top-label">Cities</span>
+	</div>
+	<div>
+		<span class="number top-number">{numCounties}</span> <span class="top-label">Counties</span>
+	</div>
+	<div>
+		<span class="number top-number">{numStates}</span> <span class="top-label">States</span>
+	</div>
+</section>
+<!-- divider -->
+
+<hr style="border-top: 1px solid rgba(45, 45, 45, 0.35);" />
+
+<table cellpadding="0" cellspacing="0" border="0">
+	<colgroup>
+		<col style="width: 20px" />
+		<col style="width: auto" />
+	</colgroup>
+	<tbody>
+		<!-- # of Cities -->
+		<!-- <tr>
+			<td class="number">{numCities}</td>
+			<td class="label"> Cities</td>
+		</tr> -->
+		<!-- divider -->
+		<!-- <tr>
+			<td colspan="2">
+				<hr />
+			</td>
+		</tr> -->
+		<!-- # of Counties -->
+		<!-- <tr>
+			<td class="number">{numCounties}</td>
+			<td class="label"> Counties</td>
+		</tr> -->
+		<!-- divider -->
+		<!-- <tr>
+			<td colspan="2">
+				<hr />
+			</td>
+		</tr> -->
+		<!-- # of States -->
+		<!-- <tr>
+			<td class="number">{numStates}</td>
+			<td class="label"> States</td>
+		</tr> -->
+
+		<!-- note about filters -->
+		<tr>
+			<td colspan="2" class="filters-note"
+				>To see the cities to which the following criteria apply, toggle the respective filters on
+				the map.</td
+			>
+		</tr>
+
+		<!-- # with reports -->
+		<tr>
+			<td class="number">{numReports}</td>
+			<td class="label filters"> Cities have released a report</td>
+		</tr>
+		<!-- divider -->
+		<tr>
+			<td colspan="2">
+				<hr />
+			</td>
+		</tr>
+
+		<!-- # with funding -->
+		<tr>
+			<td class="number">{numFunding}</td>
+			<td class="label filters"> Cities have approved funding</td>
+		</tr>
+		<!-- divider -->
+		<tr>
+			<td colspan="2">
+				<hr />
+			</td>
+		</tr>
+
+		<!-- # with funding source -->
+		<tr>
+			<td class="number">{numSource}</td>
+			<td class="label filters"> Cities have a funding source</td>
+		</tr>
+		<!-- divider -->
+		<tr>
+			<td colspan="2">
+				<hr />
+			</td>
+		</tr>
+
+		<!-- # with allocation -->
+		<tr>
+			<td class="number">{numAllocation}</td>
+			<td class="label filters"> Cities have started allocating</td>
+		</tr>
+		<!-- divider -->
+		<tr>
+			<td colspan="2">
+				<hr />
+			</td>
+		</tr>
+
+		<!-- # with direct payments -->
+		<tr>
+			<td class="number">{numDirect}</td>
+			<td class="label filters"> Cities have determined direct payments</td>
+		</tr>
+		<!-- divider -->
+		<tr>
+			<td colspan="2">
+				<hr />
+			</td>
+		</tr>
+
+		<!-- # with eligibility determined -->
+		<tr>
+			<td class="number">{numEligibility}</td>
+			<td class="label filters"> Cities have determined eligibility</td>
+		</tr>
+	</tbody>
+</table>
+
+<style>
+	.location-stats {
+		display: flex;
+		justify-content: space-between;
+		padding-bottom: 0.5rem;
+	}
+
+	.location-stats div {
+		display: flex;
+		align-items: center;
+		gap: 3px;
+	}
+
+	table {
+		width: 100%;
+	}
+
+	td {
+		vertical-align: middle;
+	}
+
+	.number {
+		font-size: 1.6rem;
+		color: var(--red);
+		text-align: right;
+		padding: 0 0.75rem 0 0;
+	}
+
+	.top-number {
+		padding: 0 0.25rem 0 0;
+	}
+
+	.label {
+		font-size: 0.95rem;
+		line-height: 1.1;
+	}
+
+	.top-label {
+		font-size: 1.1rem;
+	}
+
+	hr {
+		margin-top: 0.1rem;
+		margin-bottom: 0.1rem;
+	}
+
+	.filters-note {
+		padding-top: 0.75rem;
+		padding-bottom: 0.5rem;
+		font-family: 'Barlow Condensed', sans-serif;
+		font-size: 0.95rem;
+	}
+</style>
