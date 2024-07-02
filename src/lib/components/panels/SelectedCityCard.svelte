f4d7a268f81ad9cf36a3a2e4752048269f79d4ca
chejel <chejenj@gmail.com>
2024-07-01 22:42:23 -0400

    Modify data wrangling

--- src/lib/components/panels/SelectedCityCard.svelte
+++ src/lib/components/panels/SelectedCityCard.svelte
@@ -8,7 +8,7 @@
 	// Import icon components
 	import XIcon from '$lib/components/icons/X.svelte';
 	import CheckIcon from '$lib/components/icons/Check.svelte';
-	import LinkIcon from '$lib/components/icons/Link.svelte';
+	import ArrowExpandIcon from '$lib/components/icons/ArrowExpand.svelte';
 
 	// Rename question variables
 	$: cityData = $reparationsCityData
@@ -17,19 +17,159 @@
 			properties: {
 				City: city.properties.Location,
 				State: city.properties.State,
-				'Report released': city.properties['Has the city released a report on reparations?'],
-				'Funding approved': city.properties['Has the city approved reparations funding?'],
-				'Funding source': city.properties['What is the potential funding source?'],
-				'Allocation started': city.properties['Has the city begun allocating reparations?'],
-				'Direct payments':
-					city.properties['Has the city determined if direct payments will be included?'],
-				'Eligibility determined':
-					city.properties['Has the city determined who is eligible to receive direct payments?'],
-				'Funding addresses health': city.properties['Is any of the funding addressing health?'],
-				'Funding directed to': city.properties['Where is funding directed?'],
-				'Other topics included':
-					city.properties['What other topic areas included in the reparation approach?'],
-				'Additional notes': city.properties['Additional Notes']
+				'Report released': (() => {
+					// if the field for the question is linked, the string appears in Markdown format ([text](link)) so code below extracts the text and link
+					// and if the field has no value, the response is null
+					const question = 'Has the location released a report on reparations?';
+					const response = city.properties[question];
+					const link = response?.includes('[') ? response.match(/\[(.*?)\]\((.*?)\)/)?.[2] : null;
+
+					return {
+						question: question,
+						response: response
+							? response?.includes('[')
+								? response.match(/\[(.*?)\]\((.*?)\)/)?.[1]
+								: response.trim()
+							: null,
+						link: link
+					};
+				})(),
+				'Funding approved': (() => {
+					const question = 'Has the location approved reparations funding?';
+					const response = city.properties[question];
+					const link = response?.includes('[') ? response.match(/\[(.*?)\]\((.*?)\)/)?.[2] : null;
+
+					return {
+						question: question,
+						response: response
+							? response?.includes('[')
+								? response.match(/\[(.*?)\]\((.*?)\)/)?.[1]
+								: response.trim()
+							: null,
+						link: link
+					};
+				})(),
+				'Funding source': (() => {
+					const question = 'What is the potential funding source?';
+					const response = city.properties[question];
+					const link = response?.includes('[') ? response.match(/\[(.*?)\]\((.*?)\)/)?.[2] : null;
+
+					return {
+						question: question,
+						response: response
+							? response?.includes('[')
+								? response.match(/\[(.*?)\]\((.*?)\)/)?.[1]
+								: response.trim()
+							: null,
+						link: link
+					};
+				})(),
+				'Allocation started': (() => {
+					const question = 'Has the location begun allocating reparations?';
+					const response = city.properties[question];
+					const link = response?.includes('[') ? response.match(/\[(.*?)\]\((.*?)\)/)?.[2] : null;
+
+					return {
+						question: question,
+						response: response
+							? response?.includes('[')
+								? response.match(/\[(.*?)\]\((.*?)\)/)?.[1]
+								: response.trim()
+							: null,
+						link: link
+					};
+				})(),
+				'Direct payments': (() => {
+					const question = 'Has the location determined if direct payments will be included?';
+					const response = city.properties[question];
+					const link = response?.includes('[') ? response.match(/\[(.*?)\]\((.*?)\)/)?.[2] : null;
+
+					return {
+						question: question,
+						response: response
+							? response?.includes('[')
+								? response.match(/\[(.*?)\]\((.*?)\)/)?.[1]
+								: response.trim()
+							: null,
+						link: link
+					};
+				})(),
+				'Eligibility determined': (() => {
+					const question =
+						'Has the location determined who is eligible to receive direct payments?';
+					const response = city.properties[question];
+					const link = response?.includes('[') ? response.match(/\[(.*?)\]\((.*?)\)/)?.[2] : null;
+
+					return {
+						question: question,
+						response: response
+							? response?.includes('[')
+								? response.match(/\[(.*?)\]\((.*?)\)/)?.[1]
+								: response.trim()
+							: null,
+						link: link
+					};
+				})(),
+				'Health addressed': (() => {
+					const question = 'Is any of the funding addressing health?';
+					const response = city.properties[question];
+					const link = response?.includes('[') ? response.match(/\[(.*?)\]\((.*?)\)/)?.[2] : null;
+
+					return {
+						question: question,
+						response: response
+							? response?.includes('[')
+								? response.match(/\[(.*?)\]\((.*?)\)/)?.[1]
+								: response.trim()
+							: null,
+						link: link
+					};
+				})(),
+				'Funding directed': (() => {
+					const question = 'Where is funding directed?';
+					const response = city.properties[question];
+					const link = response?.includes('[') ? response.match(/\[(.*?)\]\((.*?)\)/)?.[2] : null;
+
+					return {
+						question: question,
+						response: response
+							? response?.includes('[')
+								? response.match(/\[(.*?)\]\((.*?)\)/)?.[1]
+								: response.trim()
+							: null,
+						link: link
+					};
+				})(),
+				'Other topics': (() => {
+					const question = 'What other topic areas included in the reparation approach?';
+					const response = city.properties[question];
+					const link = response?.includes('[') ? response.match(/\[(.*?)\]\((.*?)\)/)?.[2] : null;
+
+					return {
+						question: question,
+						response: response
+							? response?.includes('[')
+								? response.match(/\[(.*?)\]\((.*?)\)/)?.[1]
+								: response.trim()
+							: null,
+						link: link
+					};
+				})(),
+				'Additional notes': (() => {
+					const question = 'Additional Notes';
+					const response = city.properties[question];
+					const link = response?.includes('[') ? response.match(/\[(.*?)\]\((.*?)\)/)?.[2] : null;
+
+					return {
+						question: question,
+						response: response
+							? response?.includes('[')
+								? response.match(/\[(.*?)\]\((.*?)\)/)?.[1]
+								: response.trim()
+							: null,
+						link: link
+					};
+				})()
 			}
 		}))
 		.find((city) => city.properties.City === $selectedCity);
@@ -49,35 +189,59 @@
 <hr />
 
 <!-- City details -->
-<!-- && city[0] !== 'Link' -->
-<div class="city-info">
+
+<table class="city-info">
 	{#each Object.entries(cityData.properties).filter((city) => city[0] !== 'City' && city[0] !== 'State') as city}
-		<table class="questions">
+		{#if city[0] !== 'Funding directed' && city[0] !== 'Other topics' && city[0] !== 'Additional notes'}
 			<tr>
-				<td
-					>{#if city[1].includes('No')}
+				<td>
+					{#if city[1].response?.includes('No')}
 						<XIcon />
 					{:else}
 						<CheckIcon />
 					{/if}</td
 				>
 				<td
-					><p>
-						<span class="question-bold">{city[0]}</span>: {city[1]}
+					><p class="qAndA">
+						<span class="question-bold">
+							{city[1].question}
+						</span>
+						<span class="response"
+							>{#if city[1].link}
+								{city[1].response}
+								<a href={city[1].link} class="link-btn" aria-label="See related link"
+									><ArrowExpandIcon /> Source</a
+								>
+							{:else}
+								{city[1].response}
+							{/if}
+						</span>
 					</p></td
 				>
 			</tr>
-		</table>
+		{:else}
+			<tr class="additional">
+				<td>
+					<p class="qAndA">
+						<span class="question-bold">
+							{city[1].question}
+						</span>
+						<span class="response"
+							>{#if city[1].link}
+								{city[1].response}
+								<a href={city[1].link} class="link-btn" aria-label="See related link"
+									><ArrowExpandIcon /> Source</a
+								>
+							{:else}
+								{city[1].response}
+							{/if}
+						</span>
+					</p>
+				</td>
+			</tr>
+		{/if}
 	{/each}
-</div>
-
-<!-- Link -->
-<!-- <div class="article-link">
-	<LinkIcon /><span><span class="question-bold">Article</span>:</span>
-	<a href={cityData.properties.Link}
-		>{cityData.properties.Link.match(/^(?:https?:\/\/)?(?:www\.)?([^\/]+)/)[1]}</a
-	>
-</div> -->
+</table>
 
 <style>
 	.header {
@@ -95,18 +259,33 @@
 	}
 
 	.city-info {
-		position: relative;
-		z-index: 2;
+		display: flex;
+		flex-direction: column;
+		/* column-gap: 5px; */
+		/* margin-bottom: 0.5rem; */
 	}
 
-	.questions {
-		display: flex;
-		column-gap: 5px;
-		margin-bottom: 0.5rem;
+	.city-info > tr {
+		margin-bottom: 0.9rem;
+	}
+
+	tr.additional {
+		background-color: #eee;
+		width: 100%;
+		margin-bottom: 0;
+		padding-top: 1rem;
+		padding-right: 1rem;
+		padding-left: 1rem;
+	}
+
+	tr.additional:last-child {
+		padding-bottom: 1rem;
 	}
 
 	.question-bold {
-		font-weight: 600;
+		font-weight: 500;
+		line-height: 1.15;
+		font-family: 'Barlow Condensed', sans-serif;
 	}
 
 	tr {
@@ -116,13 +295,35 @@
 
 	.map-container {
 		position: absolute;
-		top: 200px;
-		right: 10px;
+		top: 220px;
+		right: 40px;
 		width: 25%;
 		z-index: 1;
 	}
 
 	hr {
 		padding-bottom: 0.75rem;
+	}
+
+	.qAndA {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.response {
+		display: flex;
+		flex-direction: column;
+		row-gap: 2px;
+		margin-top: 1px;
+	}
+
+	.link-btn {
+		display: flex;
+		align-items: center;
+		column-gap: 2px;
+		text-transform: uppercase;
+		text-decoration: none;
+		font-size: 0.8rem;
+		font-weight: 700;
 	}
 </style>
--- src/lib/components/panels/Stats.svelte
+++ src/lib/components/panels/Stats.svelte
@@ -9,53 +9,66 @@
 	const numStates = $reparationsStateData?.length;
 
 	// # with reports
-	const numReports = $reparationsCityData?.filter((d) =>
-		d.properties['Has the city released a report on reparations?'].includes('Yes')
-	).length;
+	const numReports = $reparationsCityData
+		?.filter((d) => d.properties['Has the location released a report on reparations?'])
+		.filter((d) =>
+			d.properties['Has the location released a report on reparations?'].includes('Yes')
+		).length;
 
 	// # with funding
-	const numFunding = $reparationsCityData?.filter((d) =>
-		d.properties['Has the city approved reparations funding?'].includes('Yes')
-	).length;
+	const numFunding = $reparationsCityData
+		?.filter((d) => d.properties['Has the location approved reparations funding?'])
+		.filter((d) =>
+			d.properties['Has the location approved reparations funding?'].includes('Yes')
+		).length;
 
 	// # with funding source
-	const numSource = $reparationsCityData?.filter((d) =>
-		d.properties['What is the potential funding source?'].includes('Yes')
-	).length;
+	const numSource = $reparationsCityData
+		?.filter((d) => d.properties['What is the potential funding source?'])
+		.filter((d) => d.properties['What is the potential funding source?'].includes('Yes')).length;
 
 	// # with allocation
-	const numAllocation = $reparationsCityData?.filter((d) =>
-		d.properties['Has the city begun allocating reparations?'].includes('Yes')
-	).length;
+	const numAllocation = $reparationsCityData
+		?.filter((d) => d.properties['Has the location begun allocating reparations?'])
+		.filter((d) =>
+			d.properties['Has the location begun allocating reparations?'].includes('Yes')
+		).length;
 
 	// # with direct payments
-	const numDirect = $reparationsCityData?.filter((d) =>
-		d.properties['Has the city determined if direct payments will be included?'].includes('Yes')
-	).length;
+	const numDirect = $reparationsCityData
+		?.filter(
+			(d) => d.properties['Has the location determined if direct payments will be included?']
+		)
+		.filter((d) =>
+			d.properties['Has the location determined if direct payments will be included?'].includes(
+				'Yes'
+			)
+		).length;
 
 	// # with eligibility determined
-	const numEligibility = $reparationsCityData?.filter((d) =>
-		d.properties['Has the city determined who is eligible to receive direct payments?'].includes(
-			'Yes'
+	const numEligibility = $reparationsCityData
+		?.filter(
+			(d) => d.properties['Has the location determined who is eligible to receive direct payments?']
 		)
-	).length;
+		.filter((d) =>
+			d.properties[
+				'Has the location determined who is eligible to receive direct payments?'
+			].includes('Yes')
+		).length;
 </script>
 
 <section class="location-stats">
 	<div>
 		<span class="number top-number">{numCities}</span> <span class="top-label">Cities</span>
 	</div>
-	<!-- <div>
-		<span class="number top-number">{numCounties}</span> <span class="top-label">Counties</span>
-	</div> -->
 	<span style="font-size: 1.5rem; font-weight: 400; color: silver;">|</span>
 	<div>
 		<span class="number top-number" style="color: #588157;">{numStates}</span>
 		<span class="top-label">States</span>
 	</div>
 </section>
-<!-- divider -->
 
+<!-- divider -->
 <hr style="border-top: 0.5px solid rgba(45, 45, 45, 0.75);" />
 
 <table cellpadding="0" cellspacing="0" border="0">
@@ -64,34 +77,6 @@
 		<col style="width: auto" />
 	</colgroup>
 	<tbody>
-		<!-- # of Cities -->
-		<!-- <tr>
-			<td class="number">{numCities}</td>
-			<td class="label"> Cities</td>
-		</tr> -->
-		<!-- divider -->
-		<!-- <tr>
-			<td colspan="2">
-				<hr />
-			</td>
-		</tr> -->
-		<!-- # of Counties -->
-		<!-- <tr>
-			<td class="number">{numCounties}</td>
-			<td class="label"> Counties</td>
-		</tr> -->
-		<!-- divider -->
-		<!-- <tr>
-			<td colspan="2">
-				<hr />
-			</td>
-		</tr> -->
-		<!-- # of States -->
-		<!-- <tr>
-			<td class="number">{numStates}</td>
-			<td class="label"> States</td>
-		</tr> -->
-
 		<!-- note about filters -->
 		<tr>
 			<td colspan="2" class="filters-note"
