7ed19cbb7facab8740ce7154458160bc1897a9fd
chejel <chejenj@gmail.com>
2024-09-30 23:23:53 -0400

    Add modal for mobile landscape msg

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,35 @@
+<script>
+	export let showModal; // boolean
+
+	let dialog; // HTMLDialogElement
+
+	$: if (dialog && showModal) dialog.showModal();
+</script>
+
+<!-- Source: https://svelte.dev/examples/modal -->
+<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
+<dialog bind:this={dialog}>
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div>
+		<slot />
+	</div>
+</dialog>
+
+<style>
+	dialog {
+		margin: auto;
+		background-color: #333;
+		color: white;
+		border-radius: 3px;
+		border: none;
+		text-align: center;
+	}
+
+	dialog::backdrop {
+		background: rgba(0, 0, 0, 0.4);
+	}
+
+	dialog > div {
+		padding: 1rem;
+	}
+</style>
