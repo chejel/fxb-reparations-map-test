bfb34494abeaa50f0c7135fe102751f4b20fbf70
chejel <chejenj@gmail.com>
2025-02-19 17:48:47 -0500

    Modify Stats component

--- src/lib/components/panels/Stats.svelte
+++ src/lib/components/panels/Stats.svelte
@@ -15,77 +15,81 @@
 	const wordsIncluded = ['yes', 'but'];
 
 	// Conditional for selected geography tab (cities, counties, states)
-	let selectedTab = $reparationsCityData;
+	//let selectedTabData = $reparationsCityData;
 	let citiesTab = true;
 	let countiesTab = false;
 	let statesTab = false;
 
-	$: if (citiesTab === true) {
-		selectedTab = $reparationsCityData;
-	} else if (countiesTab === true) {
-		selectedTab = $reparationsCountyData;
-	} else if (statesTab === true) {
-		selectedTab = $reparationsStateData;
+	// $: selectedTabData = citiesTab
+	// 	? $reparationsCityData
+	// 	: countiesTab
+	// 		? $reparationsCountyData
+	// 		: $reparationsStateData;
+
+	let numReports;
+	filterByProperty($reparationsCityData);
+
+	function filterByProperty(dataset) {
+		numReports = dataset
+			?.filter((d) => d.properties['Has the location released a report on reparations?'])
+			.filter((d) =>
+				['yes'].some((value) =>
+					d.properties['Has the location released a report on reparations?']
+						.toLowerCase()
+						.includes(value)
+				)
+			).length;
 	}
 
-	// # with reports
-	$: numReports = selectedTab
-		?.filter((d) => d.properties['Has the location released a report on reparations?'])
-		.filter((d) =>
-			d.properties['Has the location released a report on reparations?'].includes('Yes')
-		).length;
+	//$: numReports = filterByProperty('Has the location released a report on reparations?', ['yes']);
+	// $: numFunding = filterByProperty('Has the location approved reparations funding?', ['yes']);
+	// $: numSource = filterByProperty('What is the potential funding source?', ['yes']);
+	// $: numAllocation = filterByProperty('Has the location begun allocating reparations?', ['yes']);
+	// $: numDirect = filterByProperty(
+	// 	'Has the location determined if direct payments will be included?',
+	// 	['yes']
+	// );
+	// $: numEligibility = filterByProperty(
+	// 	'Has the location determined who is eligible to receive direct payments?',
+	// 	wordsIncluded
+	// );
+	// $: numHealth = filterByProperty('Is any of the funding addressing health?', wordsIncluded);
 
-	// # with funding
-	$: numFunding = selectedTab
-		?.filter((d) => d.properties['Has the location approved reparations funding?'])
-		.filter((d) =>
-			d.properties['Has the location approved reparations funding?'].includes('Yes')
-		).length;
+	// Questions
+	$: questions = [
+		{ count: numReports, label: 'released a report' }
+		// { count: numFunding, label: 'approved funding' },
+		// { count: numSource, label: 'have a funding source' },
+		// { count: numAllocation, label: 'started allocating reparations' },
+		// { count: numDirect, label: 'determined direct payments' },
+		// { count: numEligibility, label: 'determined eligibility' },
+		// { count: numHealth, label: 'address health' }
+	];
 
-	// # with funding source
-	$: numSource = selectedTab
-		?.filter((d) => d.properties['What is the potential funding source?'])
-		.filter((d) => d.properties['What is the potential funding source?'].includes('Yes')).length;
-
-	// # with allocation
-	$: numAllocation = selectedTab
-		?.filter((d) => d.properties['Has the location begun allocating reparations?'])
-		.filter((d) =>
-			d.properties['Has the location begun allocating reparations?'].includes('Yes')
-		).length;
-
-	// # with direct payments
-	$: numDirect = selectedTab
-		?.filter(
-			(d) => d.properties['Has the location determined if direct payments will be included?']
-		)
-		.filter((d) =>
-			d.properties['Has the location determined if direct payments will be included?'].includes(
-				'Yes'
-			)
-		).length;
-
-	// # with eligibility determined
-	$: numEligibility = selectedTab
-		?.filter(
-			(d) => d.properties['Has the location determined who is eligible to receive direct payments?']
-		)
-		.filter((d) =>
-			wordsIncluded.some((word) =>
-				d.properties['Has the location determined who is eligible to receive direct payments?']
-					.toLowerCase()
-					.includes(word)
-			)
-		).length;
-
-	// # with health
-	$: numHealth = selectedTab
-		?.filter((d) => d.properties['Is any of the funding addressing health?'])
-		.filter((d) =>
-			wordsIncluded.some((word) =>
-				d.properties['Is any of the funding addressing health?'].toLowerCase().includes(word)
-			)
-		).length;
+	// Tabs
+	// const tabs = [
+	// 	{
+	// 		label: 'Cities',
+	// 		count: numCities,
+	// 		active: citiesTab,
+	// 		color: 'rgba(var(--red), 0.85)',
+	// 		borderColor: 'rgba(var(--red), 1)'
+	// 	},
+	// 	{
+	// 		label: 'Counties',
+	// 		count: numCounties,
+	// 		active: countiesTab,
+	// 		color: 'rgba(var(--dark-green), 0.75)',
+	// 		borderColor: 'rgba(var(--green), 1)'
+	// 	},
+	// 	{
+	// 		label: 'States',
+	// 		count: numStates,
+	// 		active: statesTab,
+	// 		color: 'rgba(var(--green), 1)',
+	// 		borderColor: 'rgba(var(--green), 1)'
+	// 	}
+	// ];
 </script>
 
 <!-- note about filters -->
@@ -102,6 +106,7 @@
 			citiesTab = true;
 			countiesTab = false;
 			statesTab = false;
+			filterByProperty($reparationsCityData);
 		}}
 	>
 		<span class="number top-number" style="color: rgba(var(--red), 0.85);">{numCities}</span>
@@ -116,6 +121,7 @@
 			citiesTab = false;
 			countiesTab = true;
 			statesTab = false;
+			filterByProperty($reparationsCountyData);
 		}}
 	>
 		<span class="number top-number" style="color: rgba(var(--dark-green), 0.75);"
@@ -132,6 +138,7 @@
 			citiesTab = false;
 			countiesTab = false;
 			statesTab = true;
+			filterByProperty($reparationsStateData);
 		}}
 	>
 		<span class="number top-number" style="color: rgba(var(--green), 1);">{numStates}</span>
@@ -139,242 +146,87 @@
 	</button>
 </div>
 
+<!-- <div class="location-stats">
+	{#each tabs as tab}
+		<button
+			class:active={citiesTab}
+			style={citiesTab ? `border-top: 2px solid ${tab.borderColor}` : ''}
+			on:click|stopPropagation={() => {
+				// citiesTab = tab.label === 'Cities';
+				// countiesTab = tab.label === 'Counties';
+				// statesTab = tab.label === 'States';
+				// console.log(tab.label);
+
+				// if(tab.label === "Cities") {
+				// 	selectedTabData = reparationsCityData;
+				// } else if(tab.label === "Counties") {
+				// 	selectedTabData = reparationsCountyData;
+				// } else {
+				// 	selectedTabData = reparationsStateData;
+				// }
+				tab.active === true;
+				console.log(tab);
+				if (!citiesTab && tab.label === 'Cities') {
+					citiesTab = true;
+					countiesTab = false;
+					statesTab = false;
+				} else if (tab.label === 'Counties') {
+					citiesTab = false;
+					countiesTab = true;
+					statesTab = false;
+				} else if (tab.label === 'States') {
+					citiesTab = false;
+					countiesTab = false;
+					statesTab = true;
+				}
+				filterByProperty();
+			}}
+		>
+			<span class="number top-number" style={`color: ${tab.color};`}>{tab.count}</span>
+			<span class="top-label">{tab.label}</span>
+		</button>
+	{/each}
+</div> -->
+
 <table cellpadding="0" cellspacing="0" border="0">
 	<colgroup>
 		<col style="width: 20px" />
 		<col style="width: auto" />
 	</colgroup>
 	<tbody>
-		<!-- divider -->
-		<tr>
-			<td colspan="2">
-				<hr class="outer" style="margin-top: 0;" />
-			</td></tr
-		>
-		<!-- # with reports -->
-		<tr>
-			<td
-				class="number"
-				style={citiesTab
-					? 'color: rgba(var(--red), 0.85)'
-					: countiesTab
-						? 'color: rgba(var(--dark-green), 0.75)'
-						: 'color: rgba(var(--green), 1)'}>{numReports}</td
-			>
-			<td class="label">
-				{citiesTab
-					? numReports.length !== 1
-						? 'cities'
-						: 'city'
-					: countiesTab
-						? numReports.length !== 1
-							? 'counties'
-							: 'county'
-						: numReports.length !== 1
-							? 'states'
-							: 'state'}
-				{numReports.length !== 1 ? 'have' : 'has'} released a report</td
-			>
-		</tr>
-		<!-- divider -->
-		<tr>
-			<td colspan="2">
-				<hr />
-			</td>
-		</tr>
-
-		<!-- # with funding -->
-		<tr>
-			<td
-				class="number"
-				style={citiesTab
-					? 'color: rgba(var(--red), 0.85)'
-					: countiesTab
-						? 'color: rgba(var(--dark-green), 0.75)'
-						: 'color: rgba(var(--green), 1)'}>{numFunding}</td
-			>
-			<td class="label">
-				{citiesTab
-					? numFunding !== 1
-						? 'cities'
-						: 'city'
-					: countiesTab
-						? numFunding !== 1
-							? 'counties'
-							: 'county'
-						: numFunding !== 1
-							? 'states'
-							: 'state'}
-				{numFunding !== 1 ? 'have' : 'has'} approved funding</td
-			>
-		</tr>
-		<!-- divider -->
-		<tr>
-			<td colspan="2">
-				<hr />
-			</td>
-		</tr>
-
-		<!-- # with funding source -->
-		<tr>
-			<td
-				class="number"
-				style={citiesTab
-					? 'color: rgba(var(--red), 0.85)'
-					: countiesTab
-						? 'color: rgba(var(--dark-green), 0.75)'
-						: 'color: rgba(var(--green), 1)'}>{numSource}</td
-			>
-			<td class="label">
-				{citiesTab
-					? numSource !== 1
-						? 'cities'
-						: 'city'
-					: countiesTab
-						? numSource !== 1
-							? 'counties'
-							: 'county'
-						: numSource !== 1
-							? 'states'
-							: 'state'}
-				{numSource !== 1 ? 'have' : 'has'} a funding source</td
-			>
-		</tr>
-		<!-- divider -->
-		<tr>
-			<td colspan="2">
-				<hr />
-			</td>
-		</tr>
-
-		<!-- # with allocation -->
-		<tr>
-			<td
-				class="number"
-				style={citiesTab
-					? 'color: rgba(var(--red), 0.85)'
-					: countiesTab
-						? 'color: rgba(var(--dark-green), 0.75)'
-						: 'color: rgba(var(--green), 1)'}>{numAllocation}</td
-			>
-			<td class="label">
-				{citiesTab
-					? numAllocation !== 1
-						? 'cities'
-						: 'city'
-					: countiesTab
-						? numAllocation !== 1
-							? 'counties'
-							: 'county'
-						: numAllocation !== 1
-							? 'states'
-							: 'state'}
-				{numAllocation !== 1 ? 'have' : 'has'} started allocating reparations</td
-			>
-		</tr>
-		<!-- divider -->
-		<tr>
-			<td colspan="2">
-				<hr />
-			</td>
-		</tr>
-
-		<!-- # with direct payments -->
-		<tr>
-			<td
-				class="number"
-				style={citiesTab
-					? 'color: rgba(var(--red), 0.85)'
-					: countiesTab
-						? 'color: rgba(var(--dark-green), 0.75)'
-						: 'color: rgba(var(--green), 1)'}>{numDirect}</td
-			>
-			<td class="label">
-				{citiesTab
-					? numDirect !== 1
-						? 'cities'
-						: 'city'
-					: countiesTab
-						? numDirect !== 1
-							? 'counties'
-							: 'county'
-						: numDirect !== 1
-							? 'states'
-							: 'state'}
-				{numDirect !== 1 ? 'have' : 'has'} determined direct payments</td
-			>
-		</tr>
-		<!-- divider -->
-		<tr>
-			<td colspan="2">
-				<hr />
-			</td>
-		</tr>
-
-		<!-- # with eligibility determined -->
-		<tr>
-			<td
-				class="number"
-				style={citiesTab
-					? 'color: rgba(var(--red), 0.85)'
-					: countiesTab
-						? 'color: rgba(var(--dark-green), 0.75)'
-						: 'color: rgba(var(--green), 1)'}>{numEligibility}</td
-			>
-			<td class="label">
-				{citiesTab
-					? numEligibility !== 1
-						? 'cities'
-						: 'city'
-					: countiesTab
-						? numEligibility !== 1
-							? 'counties'
-							: 'county'
-						: numEligibility !== 1
-							? 'states'
-							: 'state'}
-				{numEligibility !== 1 ? 'have' : 'has'} determined eligibility</td
-			>
-			<!-- <td class="label"> cities address eligibility</td> -->
-		</tr>
-		<!-- divider -->
-		<tr>
-			<td colspan="2">
-				<hr />
-			</td>
-		</tr>
-
-		<!-- # with health -->
-		<tr>
-			<td
-				class="number"
-				style={citiesTab
-					? 'color: rgba(var(--red), 0.85)'
-					: countiesTab
-						? 'color: rgba(var(--dark-green), 0.75)'
-						: 'color: rgba(var(--green), 1)'}>{numHealth}</td
-			>
-			<td class="label">
-				{citiesTab
-					? numHealth.length !== 1
-						? 'cities'
-						: 'city'
-					: countiesTab
-						? numHealth.length !== 1
-							? 'counties'
-							: 'county'
-						: numHealth.length !== 1
-							? 'states'
-							: 'state'}
-				{numHealth.length !== 1 ? 'address' : 'addresses'} health</td
-			>
-		</tr>
-		<!-- divider -->
-		<!-- <tr>
-			<td colspan="2">
-				<hr class="outer" />
-			</td></tr
-		> -->
+		{#each questions as item}
+			<!-- divider -->
+			<tr>
+				<td colspan="2">
+					<hr class="outer" style="margin-top: 0;" />
+				</td>
+			</tr>
+			<tr>
+				<td
+					class="number"
+					style={citiesTab
+						? 'color: rgba(var(--red), 0.85)'
+						: countiesTab
+							? 'color: rgba(var(--dark-green), 0.75)'
+							: 'color: rgba(var(--green), 1)'}>{item.count}</td
+				>
+				<td class="label">
+					{citiesTab
+						? item.count !== 1
+							? 'cities'
+							: 'city'
+						: countiesTab
+							? item.count !== 1
+								? 'counties'
+								: 'county'
+							: item.count !== 1
+								? 'states'
+								: 'state'}
+					{item.count !== 1 ? 'have' : 'has'}
+					{item.label}</td
+				>
+			</tr>
+		{/each}
 	</tbody>
 </table>
 
