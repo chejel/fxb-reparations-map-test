c6c7a3cb01ecfa6d42f7a43c77b65e33daa723bf
chejel <chejenj@gmail.com>
2025-02-26 17:33:58 -0500

    Add map directions

--- src/lib/components/panels/About.svelte
+++ src/lib/components/panels/About.svelte
@@ -1,7 +1,8 @@
 <script>
 	// Import components
 	import Stats from '$lib/components/panels/Stats.svelte';
-	import Legend from '$lib/components/panels/Legend.svelte';
+	// import Legend from '$lib/components/panels/Legend.svelte';
+	import MapMarkerIcon from '$lib/components/icons/MapMarker.svelte';
 
 	// Import icon components
 	import MarkerIcon from '$lib/components/icons/MapMarker.svelte';
@@ -32,11 +33,46 @@
 	</p>
 
 	<p>This project was made possible through funding from the Robert Wood Johnson Foundation.</p>
-	<hr />
-	<p style="font-size: 0.8rem;">
-		Zoom in or double click on the map to see any city names that do not initially appear.
-	</p>
-	<div class="legend-container">
+
+	<div
+		style="background-color: rgba(var(--gray), 0.5); padding: 0.6rem 0.75rem; font-size: 0.8rem; border-radius: 1px;"
+	>
+		To add a location to the map, please <a
+			href="https://airtable.com/appVAaDUbHug6JBVT/shrsYhYfRAMFTt9Cw"
+			target="_blank"><strong>complete this submission form</strong></a
+		>. Once approved, it will be added to the map and directory.
+	</div>
+	<!-- <hr /> -->
+	<div style="margin: 2rem 0;">
+		<span class="card-header" style="background-color: transparent; padding: 0;"
+			><MapMarkerIcon />Directions</span
+		>
+		<hr />
+		<ul style="padding-left: 1rem;">
+			<li>
+				Select a <span style="color: rgba(var(--red), 1); font-weight: 700;">city</span>,
+				<span style="color: rgba(var(--dark-green), 0.75); font-weight: 700;">county</span> or
+				<span style="color: rgba(var(--green), 1); font-weight: 700;">state</span>
+				on the map to see more information about reparations efforts at the location.
+			</li>
+			<li>
+				Zoom in or double click on the map to see any geographic names that do not initially appear.
+			</li>
+			<li>
+				To see a list of all the locations represented on the map, select the <span
+					style="font-family: 'Barlow Condensed', sans-serif; text-transform: uppercase; font-weight: 500; background-color: rgba(var(--light-gray), 1); padding: 0px 5px; border-radius: 1px; border: 0.5px solid rgba(var(--black), 1);"
+					>List of Locations</span
+				>
+				tab above.
+			</li>
+		</ul>
+		<hr />
+	</div>
+</section>
+
+<section aria-label="Legend and data stats">
+	<!-- Legend -->
+	<!-- <div class="legend-container">
 		<p class="legend-header">
 			<MarkerIcon />
 			<span><span style="font-weight: 600;">Map legend</span> of reparation efforts</span>
@@ -46,27 +82,24 @@
 			<span><Legend geoType="county" text="County-level" /></span>
 			<span><Legend geoType="state" text="State-level" /></span>
 		</div>
-	</div>
+	</div> -->
+
+	<!-- Stats card -->
+	{#if $reparationsCityData}
+		<div class="card">
+			<!-- header -->
+			<p class="card-header"><StatsIcon /> Locations Represented</p>
+
+			<!-- stats -->
+			<div class="stats-table">
+				<Stats />
+			</div>
+		</div>
+	{/if}
 </section>
 
-<!-- Stats card -->
-{#if $reparationsCityData}
-	<section
-		aria-label="Stats about the number of cities, counties and states on the map"
-		class="card"
-	>
-		<!-- header -->
-		<p class="card-header"><StatsIcon /> Locations Represented</p>
-
-		<!-- stats -->
-		<div class="stats-table">
-			<Stats />
-		</div>
-	</section>
-{/if}
-
 <style>
-	.legend-container {
+	/* .legend-container {
 		margin-top: 10px;
 		font-size: 0.85rem;
 		font-family: 'Barlow Condensed', sans-serif;
@@ -76,9 +109,9 @@
 		justify-content: space-between;
 		border: 0.75px solid rgba(var(--dark-green), 1);
 		border-radius: 5px;
-	}
+	} */
 
-	.legend-header {
+	/* .legend-header {
 		padding: 7px 10px;
 		text-transform: uppercase;
 		background-color: rgba(var(--yellow-orange), 0.75);
@@ -86,15 +119,14 @@
 		gap: 2px;
 		align-items: center;
 		border-radius: 5px 5px 0 0;
-	}
+	} */
 
-	.legend {
+	/* .legend {
 		padding: 7px 10px;
 		display: flex;
 		align-items: center;
-		/* justify-content: space-between; */
 		gap: 25px;
-	}
+	} */
 
 	/* stats */
 	.card-header {
