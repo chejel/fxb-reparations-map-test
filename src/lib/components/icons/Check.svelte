77bb02c18ae024e557fb1cb100baec8bd2df5ef6
chejel <chejenj@gmail.com>
2025-03-26 13:36:04 -0400

    Modify check icon for filters applied tags

--- src/lib/components/icons/Check.svelte
+++ src/lib/components/icons/Check.svelte
@@ -1,15 +1,23 @@
+<script>
+	export let width = '24px';
+	export let height = '24px';
+	export let viewBox = '0 0 30 30';
+	export let fillColor = '#0D9276';
+	export let strokeWidth = '3';
+</script>
+
 <!-- Source: https://iconoir.com/ --><svg
-	width="24px"
-	height="24px"
-	stroke-width="3"
-	viewBox="0 0 30 30"
+	aria-hidden="true"
+	{width}
+	{height}
+	{viewBox}
 	fill="none"
 	xmlns="http://www.w3.org/2000/svg"
 	color="#000000"
 	><path
 		d="M5 13L9 17L19 7"
-		stroke="#0D9276"
-		stroke-width="3"
+		stroke={fillColor}
+		stroke-width={strokeWidth}
 		stroke-linecap="round"
 		stroke-linejoin="round"
 	></path></svg
